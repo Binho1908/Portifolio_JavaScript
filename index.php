df8854a7bae590549840338e7f2b2fd27cdefd20
binho1908 <fabiosenninhaddt@gmail.com>
2021-01-18 15:58:16 -0300

    criado layout e estilo da pagina

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-AU-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+    <title>Curso php</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>JavaScript</h1>
+        <h2>Portifólio Projetos Básico</h2>
+    </header>
+    
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+                <div class="modulo verde">
+                    <h3>Módulo 01 - </h3>
+                    <ul>
+                        <li>
+                            <a href="#">
+                            Edit - 1
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                
+                <div class="modulo vermelho">
+                    <h3>Módulo 01 - </h3>
+                    <ul>
+                        <li>
+                            <a href="#">
+                            Edit - 1
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo azul">
+                    <h3>Módulo 01 - </h3>
+                    <ul>
+                        <li>
+                            <a href="#">
+                            Edit - 1
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo roxo">
+                    <h3>Módulo 01 - </h3>
+                    <ul>
+                        <li>
+                            <a href="#">
+                            Edit - 1
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo laranja">
+                    <h3>Módulo 01 - </h3>
+                    <ul>
+                        <li>
+                            <a href="#">
+                            Edit - 1
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo verde-escuro">
+                    <h3>Módulo 01 - </h3>
+                    <ul>
+                        <li>
+                            <a href="#">
+                            Edit - 1
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                
+            </nav>
+        </div>
+    </main>
+    
+    <footer class="rodape">
+    C0D3R & ALUNOS © <?= date('Y');?> 
+    </footer>
+</body>
+</html>
